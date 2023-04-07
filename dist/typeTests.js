3494b36377fb60da933c3825496aad4c9e2356e0
root <prostgles@protonmail.com>
2023-04-07 09:15:29 +0000

    change types

--- dist/typeTests.js
+++ dist/typeTests.js
@@ -1,12 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const dbo = 1;
-const filter = {};
-const filterCheck = (f) => { };
-filterCheck(filter);
-const sub = dbo.table1.count;
-const f = (a) => { };
-// f(dbo.table1)
-const ra = (a) => {
-};
-// const dboBasic: DBHandlerClient = dbo;
+(async () => {
+    const dbo = 1;
+    const filter = {};
+    const filterCheck = (f) => { };
+    filterCheck(filter);
+    const sub = dbo.table1.count;
+    const f = (a) => { };
+    // f(dbo.table1)
+    const ra = (a) => {
+    };
+    const dbH = 1;
+    dbH.dwadwa.findOne({ "name.@@.to_tsquery": ["abc81"] }, { select: {
+            h: { "$ts_headline_simple": ["name", { plainto_tsquery: "abc81" }] },
+            hh: { "$ts_headline": ["name", "abc81"] },
+            added: "$date_trunc_2hour",
+            addedY: { "$date_trunc_5minute": ["added"] }
+        } });
+    // const dboBasic: DBHandlerClient = dbo;
+});
--- lib/typeTests.ts
+++ lib/typeTests.ts
@@ -1,36 +1,48 @@
-import { AnyObject, DBSchema, FullFilter, TableHandler } from "prostgles-types";
-import { DBHandlerClient } from "./prostgles";
+import { AnyObject, DBSchema, FullFilter, Select, TableHandler } from "prostgles-types";
+import { DBHandlerClient, TableHandlerClient } from "./prostgles";
 
-
-// const schema: DBSchema = {
-  type GeneratedSchema = {
-  table1: {
-    columns: {
-      col1: string,
-    },
-    is_view: false,
-    select: true,
-    insert: true,
-    update: true,
-    delete: true,
-  }
-};
-
-const dbo: DBHandlerClient<GeneratedSchema> = 1 as any;
-
-const filter: FullFilter<GeneratedSchema["table1"]["columns"], GeneratedSchema> = {  };
-
-const filterCheck = <F extends FullFilter<void, void> | undefined>(f: F) => {};
-filterCheck(filter);
-
-const sub: TableHandler["count"] = dbo.table1.count
-
-const f = <A extends TableHandler>(a: A) => {};
-
-// f(dbo.table1)
-const ra = <A extends AnyObject>(a: A) => {
-
-}; 
-
-
-// const dboBasic: DBHandlerClient = dbo;
+(async () => {
+  // const schema: DBSchema = {
+    type GeneratedSchema = {
+    table1: {
+      columns: {
+        col1: string,
+      },
+      is_view: false,
+      select: true,
+      insert: true,
+      update: true,
+      delete: true,
+    }
+  };
+  
+  const dbo: DBHandlerClient<GeneratedSchema> = 1 as any;
+  
+  const filter: FullFilter<GeneratedSchema["table1"]["columns"], GeneratedSchema> = {  };
+  
+  const filterCheck = <F extends FullFilter<void, void> | undefined>(f: F) => {};
+  filterCheck(filter);
+  
+  const sub: TableHandler["count"] = dbo.table1.count
+  
+  const f = <A extends TableHandler>(a: A) => {};
+  
+  // f(dbo.table1)
+  const ra = <A extends AnyObject>(a: A) => {
+  
+  }; 
+  
+  const dbH: DBHandlerClient = 1 as any;
+   
+  dbH.dwadwa.findOne!(
+    { "name.@@.to_tsquery": ["abc81"] }, 
+    { select: { 
+      h: { "$ts_headline_simple": ["name", { plainto_tsquery: "abc81" }] },
+      hh: { "$ts_headline": ["name", "abc81"] } ,
+      added: "$date_trunc_2hour",
+      addedY: { "$date_trunc_5minute": ["added"] }
+    }});
+   
+  // const dboBasic: DBHandlerClient = dbo;
+  
+})
